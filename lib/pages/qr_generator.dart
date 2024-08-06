0dbbb6e95cb5646e03ee95743771003a7adeaaa2
AzzamHussain <azzamhussain70@gmail.com>
2024-08-07 01:35:45 +0500

    qr generator

---
+++ lib/pages/qr_generator.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
+
+class QrGenerator extends StatefulWidget {
+  const QrGenerator({super.key});
+
+  @override
+  State<QrGenerator> createState() => _QrGeneratorState();
+}
+
+class _QrGeneratorState extends State<QrGenerator> {
+  String? qrdata;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Generate QR Here"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.popAndPushNamed(context, "/scan");
+              },
+              icon: const Icon(Icons.qr_code_scanner))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextField(
+              onSubmitted: (value) {
+                setState(() {
+                  qrdata = value;
+                });
+              },
+            ),
+            if(qrdata!=null) PrettyQrView.data(data: qrdata!),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/qr_scanner.dart
+++ lib/pages/qr_scanner.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -26,12 +27,24 @@ class _QrScannerState extends State<QrScanner> {
       ),
       body: MobileScanner(
         controller: MobileScannerController(
-            detectionSpeed: DetectionSpeed.noDuplicates,
-            returnImage: true),
-
+            detectionSpeed: DetectionSpeed.noDuplicates, returnImage: true),
         onDetect: (capture) {
-          final List<Barcode> barcode=capture.barcodes;
-          
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            print("Barcode found ${barcode.rawValue}");
+          }
+          if (image != null) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(barcodes.first.rawValue ?? ""),
+                  content: Image(image: MemoryImage(image)),
+                );
+              },
+            );
+          }
         },
       ),
     );
